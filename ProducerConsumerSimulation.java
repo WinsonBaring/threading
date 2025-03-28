c388b62cf898fb19c67fa423733e2fc62d83988d
Winson Baring <winsonbaring10@gmail.com>
2025-03-28 18:11:15 +0800

    change file name

--- ProducerConsumerSimulation.java
+++ ProducerConsumerSimulation.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class ProducerConsumerSimulation {
     public static void main(String[] args) {
         SharedMemory sharedMemory = new SharedMemory();
         
