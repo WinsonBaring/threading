a33dffd9d888e0ee4eef564ffe9aff4d1cb70442
Winson Baring <winsonbaring10@gmail.com>
2025-03-28 18:10:02 +0800

    merge into 1 file

--- Main.java
+++ Main.java
@@ -27,4 +27,103 @@ public class Main {
 
         scanner.close();
     }
+    
+    static class SharedMemory {
+        private int buffer_size;
+        private char[] buffer;
+        private int in;
+        private int out;
+        private boolean isComplete;
+
+        public SharedMemory() {
+            buffer_size = 5;
+            buffer = new char[buffer_size];
+            in = 0;
+            out = 0;
+            isComplete = false;
+        }
+
+        public boolean isFull() {
+            return ((in + 1) % buffer_size) == out;
+        }
+
+        public boolean isEmpty() {
+            return in == out;
+        }
+
+        public void addChar(char c) {
+            buffer[in] = c;
+            in = (in + 1) % buffer_size;
+        }
+
+        public char getChar() {
+            char c = buffer[out];
+            out = (out + 1) % buffer_size;
+            return c;
+        }
+        
+        public void setComplete() {
+            isComplete = true;
+        }
+        
+        public boolean isComplete() {
+            return isComplete && isEmpty();
+        }
+    }
+    
+    static class Producer extends Thread {
+        private SharedMemory sharedMemory;
+        private String message;
+
+        public Producer(SharedMemory sm, String msg) {
+            this.sharedMemory = sm;
+            this.message = msg;
+        }
+
+        @Override
+        public void run() {
+            for (int i = 0; i < message.length(); i++) {
+                while (sharedMemory.isFull()) {
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                sharedMemory.addChar(message.charAt(i));
+                System.out.println("Producer added: " + message.charAt(i));
+            }
+            sharedMemory.setComplete();
+            System.out.println("Producer finished");
+        }
+    }
+    
+    static class Consumer extends Thread {
+        private SharedMemory sharedMemory;
+
+        public Consumer(SharedMemory sm) {
+            this.sharedMemory = sm;
+        }
+
+        @Override
+        public void run() {
+            while (true) {
+                while (sharedMemory.isEmpty()) {
+                    if (sharedMemory.isComplete()) {
+                        System.out.println("\nConsumer finished");
+                        return;
+                    }
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                char c = sharedMemory.getChar();
+                System.out.print(c);
+                System.out.flush();
+                System.out.println("\nConsumer read: " + c);
+            }
+        }
+    }
 }
